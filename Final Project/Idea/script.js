194b1681e34e5939e57e1f03f5ea6307f3a8d6bd
Cael Church <cael.church@students.snow.edu>
2024-12-05 11:49:48 -0700

    Added Unfair Mode

--- Final Project/Idea/script.js
+++ Final Project/Idea/script.js
@@ -1,58 +1,70 @@
 let score = 0;
-var message = ["Come on, just click it", "Come on, just click it", "oooo, so close", "you almost got it", "That was close"];
+var message = [
+  "Come on, just click it",
+  "Come on, just click it",
+  "Oooo, so close",
+  "You almost got it",
+  "That was close",
+];
 
 function getRandomNumber(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+$(".run").on("mouseenter", function () {
+  let left = getRandomNumber(90, 10);
+  let top = getRandomNumber(90, 10);
+  score += 1;
 
-    $('.run').on('mouseenter', function(){
-        let left = getRandomNumber(100, 0);
-        let top = getRandomNumber(100, 0);
-        score += 1;
-
-        if ($('#medium').is(':checked')) {
-        setTimeout(() => {
-
-            $('.run').css({
-                left: `${left}%`,
-                top: `${top}%`,
-            });
-        }, 300);
-        }
-        if ($('#easy').is(':checked')) {
-        setTimeout(() => {
-
-            $('.run').css({
-                left: `${left}%`,
-                top: `${top}%`,
-            });
-        }, 500);
-        }
-        if ($('#hard').is(':checked')) {
-        setTimeout(() => {
-
-            $('.run').css({
-                left: `${left}%`,
-                top: `${top}%`,
-            });
-        }, 100);
-        }
-        
-        
-        
-        $('#score').text(score);
-        $('#message').text(`${message[getRandomNumber(0,3)]}`);
-    });
-
-$('.run').on('click', function(){
-    console.log("BTN Clicked");
-    
-    $('#success').removeClass("d-none");
-    $('#success').text(`You successfully clicked the button after ${score} tries`)
-
+  if ($("#medium").is(":checked")) {
     setTimeout(() => {
-        $('#success').addClass("d-none");
-        score = 0;
-    }, 3000);
+      $(".run").css({
+        left: `${left}%`,
+        top: `${top}%`,
+      });
+    }, 300);
+  }
+  if ($("#easy").is(":checked")) {
+    setTimeout(() => {
+      $(".run").css({
+        left: `${left}%`,
+        top: `${top}%`,
+      });
+    }, 500);
+  }
+  if ($("#hard").is(":checked")) {
+    setTimeout(() => {
+      $(".run").css({
+        left: `${left}%`,
+        top: `${top}%`,
+      });
+    }, 100);
+  }
+  if ($("#unfair").is(":checked")) {
+      setTimeout(() => {
+        $(".run").css({
+          left: `${left}%`,
+          top: `${top}%`,
+        });
+      },0);
+    }
+
+  $("#score").text(score);
+  $("#message").text(`${message[getRandomNumber(0, 3)]}`);
+});
+
+
+
+$(".run").on("click", function () {
+  console.log("BTN Clicked");
+
+  $("#success").removeClass("d-none");
+  $("#success").text(
+    `You successfully clicked the button after ${score} tries`
+  );
+  score = 0;
+
+  setTimeout(() => {
+    $("#success").addClass("d-none");
+  }, 3000);
 });
