1c3c7eb6518ebea9e9cf3363932997e2861b31c9
Cael Church <cael.church@students.snow.edu>
2024-12-05 11:38:47 -0700

    Added Difficulty

--- Final Project/Idea/script.js
+++ Final Project/Idea/script.js
@@ -1,22 +1,49 @@
 let score = 0;
+var message = ["Come on, just click it", "Come on, just click it", "oooo, so close", "you almost got it", "That was close"];
 
 function getRandomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-$('.run').on('mouseenter', function(){
-    let left = getRandomNumber(100, 0);
-    let top = getRandomNumber(100, 0);
 
-    score += 1;
+    $('.run').on('mouseenter', function(){
+        let left = getRandomNumber(100, 0);
+        let top = getRandomNumber(100, 0);
+        score += 1;
 
-    $('.run').css({
-        left: `${left}%`,
-        top: `${top}%`,
+        if ($('#medium').is(':checked')) {
+        setTimeout(() => {
+
+            $('.run').css({
+                left: `${left}%`,
+                top: `${top}%`,
+            });
+        }, 300);
+        }
+        if ($('#easy').is(':checked')) {
+        setTimeout(() => {
+
+            $('.run').css({
+                left: `${left}%`,
+                top: `${top}%`,
+            });
+        }, 500);
+        }
+        if ($('#hard').is(':checked')) {
+        setTimeout(() => {
+
+            $('.run').css({
+                left: `${left}%`,
+                top: `${top}%`,
+            });
+        }, 100);
+        }
+        
+        
+        
+        $('#score').text(score);
+        $('#message').text(`${message[getRandomNumber(0,3)]}`);
     });
-
-    $('#score').text(score);
-})
 
 $('.run').on('click', function(){
     console.log("BTN Clicked");
