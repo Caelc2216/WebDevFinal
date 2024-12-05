4f2b6be67bda9a99c29659fb26d6d3c05e8f1ace
Cael Church <cael.church@students.snow.edu>
2024-12-05 11:39:25 -0700

    Removed Clutter

--- Final Project/Idea/script.js
+++ Final Project/Idea/script.js
@@ -56,38 +56,3 @@ $('.run').on('click', function(){
         score = 0;
     }, 3000);
 });
-
-
-
-
-
-// const TETROMINOS = [
-//     { shape: [[1, 1, 1], [0, 1, 0]], color: 'cyan' }, // T shape
-//     { shape: [[1, 1], [1, 1]], color: 'yellow' }, // O shape
-//     { shape: [[1, 1, 0], [0, 1, 1]], color: 'green' }, // S shape
-//     { shape: [[0, 1, 1], [1, 1, 0]], color: 'red' }, // Z shape
-//     { shape: [[1, 1, 1, 1]], color: 'blue' }, // I shape
-//     { shape: [[1, 1, 1], [1, 0, 0]], color: 'orange' }, // L shape
-//     { shape: [[1, 1, 1], [0, 0, 1]], color: 'purple' } // J shape
-// ];
-
-//     const Grid = $('.grid');
-//     const Rows = 20;
-//     const Columns = 10;
-//     const blockSize = 30;
-
-//     let score = 0;
-//     let currentPiece;
-//     let gameInterval;
-
-// function DisplayGrid(){
-//     for (let row = 0; row < Rows; row++) {
-//         for (let col = 0; col < Columns; col++) {
-//             Grid.append(`<div class="cell"></div>`);
-//         }
-//     }
-// }
-
-// $(window).on('load', function(){
-//     DisplayGrid();
-// });
